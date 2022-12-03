9ec67258e473fdd11a80c08d679bd82f89bc5ebf
shimanekb <Brandon.j.shimanek@gmail.com>
2022-12-03 10:43:52 -0600

    updated myfirst

--- myfirst.cc
+++ myfirst.cc
@@ -15,18 +15,27 @@ int main(int argc, char* argv[])
   LogComponentEnable("UdpEchoClientApplication", LOG_LEVEL_INFO);
   LogComponentEnable("UdpEchoServerApplication", LOG_LEVEL_INFO);
 
-  NodeContainer nodes;
-  nodes.Create(2);
+  // Nodes
+  NodeContainer n1;
+  n1.Create(1);
+
+  NodeContainer n2;
+  n2.Create(1);
+
+  // Connections
+  NodeContainer n1n2;
+  n1n2.Add(n1);
+  n1n2.Add(n2);
 
   PointToPointHelper pointToPoint;
   pointToPoint.SetDeviceAttribute ("DataRate", StringValue ("5Mbps"));
   pointToPoint.SetChannelAttribute ("Delay", StringValue ("2ms"));
 
   NetDeviceContainer devices;
-  devices = pointToPoint.Install (nodes);
+  devices = pointToPoint.Install (n1n2);
 
   InternetStackHelper stack;
-  stack.Install (nodes);
+  stack.Install (n1n2);
 
   Ipv4AddressHelper address;
   address.SetBase ("10.1.1.0", "255.255.255.0");
@@ -35,7 +44,7 @@ int main(int argc, char* argv[])
 
   UdpEchoServerHelper echoServer (9);
 
-  ApplicationContainer serverApps = echoServer.Install (nodes.Get (1));
+  ApplicationContainer serverApps = echoServer.Install (n1n2.Get (1));
   serverApps.Start (Seconds (1.0));
   serverApps.Stop (Seconds (10.0));
 
@@ -44,7 +53,7 @@ int main(int argc, char* argv[])
   echoClient.SetAttribute ("Interval", TimeValue (Seconds (1.0)));
   echoClient.SetAttribute ("PacketSize", UintegerValue (1024));
 
-  ApplicationContainer clientApps = echoClient.Install (nodes.Get (0));
+  ApplicationContainer clientApps = echoClient.Install (n1n2.Get (0));
   clientApps.Start (Seconds (2.0));
   clientApps.Stop (Seconds (10.0));
 
